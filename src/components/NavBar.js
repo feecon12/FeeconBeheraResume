cbb0f3d1d90535de81d6842ab20b0aac5ef95b0f
Feecon <mail2feeconbehera@gmail.com>
2023-08-08 21:56:57 +0530

    about page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,23 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { Logo } from './Logo'
 import { useRouter } from 'next/router'
 import { TwitterIcon, GithubIcon, LinkedInIcon, PinterestIcon, DribbbleIcon, } from './Icons'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const CustomLink = ({ href, title, className = '' }) => {
     const router = useRouter();
-    
+
 
     return (
-        <Link href={href} className= {`${className} relative group`}>
+        <Link href={href} className={`${className} relative group`}>
             {title}
 
             <span className={`
              h-[1px] inline-block bg-dark 
              absolute left-0 -bottom-0.5 
              group-hover:w-full transition-[width] ease duration-300
-             ${router.asPath=== href?'w-full':'w-0'}
+             ${router.asPath === href ? 'w-full' : 'w-0'}
              `}>&nbsp;
             </span>
         </Link>
@@ -25,36 +25,56 @@ const CustomLink = ({ href, title, className = '' }) => {
 }
 
 export const NavBar = () => {
+
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
     return (
-      <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-            <nav>
-                <CustomLink href='/' title= 'Home' className='mr-4'/>
-                <CustomLink href='/about' title= 'About' className='mx-4'/>
-                <CustomLink href='/projects' title= 'Projects' className='mx-4'/>
-                <CustomLink href='/articles' title='Articles' className='ml-4'/>
-            </nav>
-            <nav className='flex items-center justify-center flex-wrap '>
-                <motion.a href='https://twitter.com' target={'_blank'} whileHover={{ y: -2 }} whileTap={ {scale:0.9}} className='w-6 mr-3'>
-                    <TwitterIcon />
-                </motion.a>
-                <motion.a href='https://github.com' target={'_blank'} whileHover={{ y: -2 }} whileTap={ {scale:0.9}} className='w-6 mx-3'>
-                   <GithubIcon/>
-                </motion.a>
-                <motion.a href='https://linkedin.com' target={'_blank'} whileHover={{ y: -2 }} whileTap={ {scale:0.9}} className='w-6 m-3'>
-                    <LinkedInIcon/>
-                </motion.a>
-                <motion.a href='' target={'_blank'} whileHover={{ y: -2 }} whileTap={ {scale:0.9}} className='w-6 mx-3'>
-                    <PinterestIcon/>
-                </motion.a>
-                <motion.a href='' target={'_blank'}whileHover={{ y: -2 }} whileTap={ {scale:0.9}} className='w-6 ml-3'>
-                    <DribbbleIcon/>
-                </motion.a>
-            </nav>
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+
+            <button className=' flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
+                <span className={`bg-dark block transistion-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+                <span className={`bg-dark block transistion-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+                <span className={`bg-dark block transistion-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+            </button>
+
+            <div className='w-full flex justify-between items-center lg:hidden'>
+                <nav>
+                    <CustomLink href='/' title='Home' className='mr-4' />
+                    <CustomLink href='/about' title='About' className='mx-4' />
+                    <CustomLink href='/projects' title='Projects' className='mx-4' />
+                    <CustomLink href='/articles' title='Articles' className='ml-4' />
+                </nav>
+
+                <nav className='flex items-center justify-center flex-wrap '>
+                    <motion.a href='https://twitter.com' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mr-3'>
+                        <TwitterIcon />
+                    </motion.a>
+                    <motion.a href='https://github.com' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'>
+                        <GithubIcon />
+                    </motion.a>
+                    <motion.a href='https://linkedin.com' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 m-3'>
+                        <LinkedInIcon />
+                    </motion.a>
+                    <motion.a href='' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 mx-3'>
+                        <PinterestIcon />
+                    </motion.a>
+                    <motion.a href='' target={'_blank'} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className='w-6 ml-3'>
+                        <DribbbleIcon />
+                    </motion.a>
+                </nav>
+            </div>
+
+
+
 
             <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-                <Logo/>
+                <Logo />
             </div>
-      </header>
-    
-  )
+        </header>
+
+    )
 }
